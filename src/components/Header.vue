e1942d1655fa6b49b2b7db78be27e8906d4d5173
FeliciaPompei <f.pompei@hotmail.com>
2022-03-30 15:33:13 +0200

    vue created, header inplace

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="container-fluid">
+    <div class="row align-items-center">
+      <div class="col-2">
+        <img class="img-fluid" src="../assets/img/logo-small.svg" alt="Spotify Logo">
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderIndex',
+}
+</script>
+
+<style scoped lang="scss">
+@import'../assets/styles/style.scss';
+
+header {
+  background-color: $headerColor;
+  img{
+    width:25%;
+    padding: 0.5rem;
+  }
+}
+</style>
